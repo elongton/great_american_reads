68417232aed9e99ba6742b80a5acfcdbda3c1f4d
Max Longton <elongton@vt.edu>
2018-05-31 22:44:25 -0400

    make it more user friendly

--- custom_map.js
+++ custom_map.js
@@ -1,41 +1,53 @@
+//////////////////////////////////////////////////////////////////////////
+/////////////////////////LABEL ARRAY DEFINITION///////////////////////////
+///////////////////////ADD DESCRIPTION EACH TIME//////////////////////////
+//////////////////////////////////////////////////////////////////////////
+infoArray = []
+var contentArray =[
+  description1,
+  description2,
+]
+//////////////////////////////////////////////////////////////////////////
+////////////////////////MARKER ARRAY DEFINITION///////////////////////////
+//////////////////////////ADD MARKER EACH TIME////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+var markers = [];
+var locations = [
+  [{lat: 37.54227, lng: -77.443538}, 'Main Branch', 'A'],
+  [{lat: 37.5543459, lng: -77.4818177}, 'Belmont', 'B']
+];
+
+
+//////////////////////////////////////////////////////////////////////////
+/////////////////////DO NOT TOUCH ANY OF THE BELOW////////////////////////
+//////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+
+
+
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 14,
     center: {lat: 37.5428458, lng: -77.442493}
   });
 
-  var contentArray =[
-    description1,
-    description2,
+for (var j = 0; j < contentArray.length; j++){
+  infoArray.push(new google.maps.InfoWindow({
+                      content: contentArray[j]
+                    })
+                )
+  }//for var j
 
-  ]
-
-
-  var infowindow1 = new google.maps.InfoWindow({
-    content: contentArray[0]
-  });
-  var infowindow2 = new google.maps.InfoWindow({
-    content: contentArray[1]
-  });
-
-  var markers = [];
-  var locations = [
-    [{lat: 37.54227, lng: -77.443538}, 'Main Branch', 'A'],
-    [{lat: 37.5543459, lng: -77.4818177}, 'Belmont', 'B']
-  ];
   function drop() {
     for (var i = 0; i < locations.length; i++) {
-      addMarkerWithTimeout(locations[i][0], i * 200, locations[i][1], locations[i][2]);
+      addMarkerWithTimeout(locations[i][0], i * 200, locations[i][1], locations[i][2], i);
     }//for
   }//function drop();
   drop();
 
-
-
-
-
-
-  function addMarkerWithTimeout(position, timeout, title, label) {
+  function addMarkerWithTimeout(position, timeout, title, label, iterator) {
     window.setTimeout(function() {
       var newMarker = new google.maps.Marker({
         position: position,
@@ -45,25 +57,18 @@ function initMap() {
         animation: google.maps.Animation.DROP
       })
       newMarker.addListener('click', function() {
-        infowindow.open(map, newMarker);
+        infoArray[iterator].open(map, newMarker);
       });
       markers.push(newMarker);
     }, timeout);
   }
 
 
-/////////////////////////////////////////////////////////////
-//////////////////////LINK LISTENERS/////////////////////////
-/////////////////////////////////////////////////////////////
   $( document ).ready(function() {
-      $('#marker1').click(function() {
-        infowindow1.open(map, markers[0]);
+    for (k=0; k < locations.length; k++){
+      $('#marker'+String(k+1)).click(function() {
+        infoArray[k].open(map, markers[k]);
       });
-      $('#marker2').click(function() {
-        infowindow2.open(map, markers[1]);
-      });
-
+    }
   });
-
-
 }//initMap()
