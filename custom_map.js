0bd26c03b05cf30007fab9b22fa0cd21ccc0cd61
Max Longton <elongton@vt.edu>
2018-05-31 21:35:56 -0400

    updated the infowindow, ready to research mass production

--- custom_map.js
+++ custom_map.js
@@ -15,7 +15,8 @@ function initMap() {
         '<div class="dialog_text_div">'+
           '<h1 class="titleHeading">And Then There Were None</h1>'+
           '<h1 class="locationHeading">Location: 101 East Franklin Street</h1>'+
-          '<h1 class="readerHeading">Reader: Patricia Parks</h1>'+
+          '<h1 class="dateHeading">Date: 07/04, 12pm-1pm</h1>'+
+          '<h1 class="readerHeading">Reader: Patty Parks</h1>'+
           '<div>'+
             '<div style="text-align: justify;">' +
               '<p style="padding-right: 10px;">Richmond Public Library was founded by civic-minded leaders in the early 1900s.  The establishment of a public library for Richmond residents took several tries.  Originally approached by Mr. Andrew Carnegie in 1901, community leaders and advocates founded the Richmond Public Library Association in 1905 and built support for a locally-funded library.</p>'+
@@ -23,7 +24,7 @@ function initMap() {
           '</div>'+
         '</div>'+ //dialog_text_div
         '<div class="dialog_image_div">'+
-          '<img src="img/book.jpg" style="" alt="book cover">' +
+          '<img src="img/and_then_there_were_none.jpg" style="" alt="book cover">' +
         '</div>'+ //dialog_image_div
       '</div>';
 
