1752fbe53222fc2c472b0162176bd55f26a6775f
Max Longton <elongton@vt.edu>
2018-05-31 21:19:47 -0400

    updated styling on dialog

--- custom_map.js
+++ custom_map.js
@@ -10,18 +10,21 @@ function initMap() {
     center: uluru
   });
 
-  var contentString = '<div id="content">'+
-      '<div id="siteNotice">'+
-      '</div>'+
-      '<h1 id="firstHeading" style="padding-bottom: 5px; margin-bottom: 0;" class="firstHeading">Location: CrossRoads</h1>'+
-      '<h2 id="secondHeading" style="padding-bottom: 5px; margin-bottom: 0;" class="secondHeading">And then there were none</h1>'+
-      '<div id="bodyContent" style="display: flex; justify-content: center;">'+
-      '<div style="width: 85%; text-align: justify;">' +
-      '<p style="padding-right: 10px;">Richmond Public Library was founded by civic-minded leaders in the early 1900s.  The establishment of a public library for Richmond residents took several tries.  Originally approached by Mr. Andrew Carnegie in 1901, community leaders and advocates founded the Richmond Public Library Association in 1905 and built support for a locally-funded library.</p>'+
-      '</div>'+
-      '<div style="width: 15%; text-align: center;" class="">' +
-      '<img src="img/book.jpg" style="height: 100px;" alt="book cover">' +
-      '</div>' +
+  var contentString =
+      '<div class="dialog">'+
+        '<div class="dialog_text_div">'+
+          '<h1 class="titleHeading">And Then There Were None</h1>'+
+          '<h1 class="locationHeading">Location: 101 East Franklin Street</h1>'+
+          '<h1 class="readerHeading">Reader: Patricia Parks</h1>'+
+          '<div>'+
+            '<div style="text-align: justify;">' +
+              '<p style="padding-right: 10px;">Richmond Public Library was founded by civic-minded leaders in the early 1900s.  The establishment of a public library for Richmond residents took several tries.  Originally approached by Mr. Andrew Carnegie in 1901, community leaders and advocates founded the Richmond Public Library Association in 1905 and built support for a locally-funded library.</p>'+
+            '</div>'+
+          '</div>'+
+        '</div>'+ //dialog_text_div
+        '<div class="dialog_image_div">'+
+          '<img src="img/book.jpg" style="" alt="book cover">' +
+        '</div>'+ //dialog_image_div
       '</div>';
 
   var infowindow = new google.maps.InfoWindow({
@@ -48,3 +51,6 @@ function initMap() {
 
 
 }
+// '<div style="width: 15%; text-align: center;" class="">' +
+//   '<img src="img/book.jpg" style="height: 100px;" alt="book cover">' +
+// '</div>' +
