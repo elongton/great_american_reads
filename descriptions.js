5ba11ab3073c85b27281b016abfcc62c1a028c22
Max Longton <elongton@vt.edu>
2018-06-27 20:31:48 -0400

    sample

--- descriptions.js
+++ descriptions.js
@@ -1,7 +1,7 @@
-var description7_4 =
+var description7_5 =
     '<div class="dialog">'+
       '<div class="dialog_text_div">'+
-        '<h1 class="titleHeading">And Then There Were None</h1>'+
+        '<h1 class="titleHeading">Patty Parks</h1>'+
         '<h1 class="locationHeading">Location: 101 East Franklin Street</h1>'+
         '<h1 class="dateHeading">Date: 07/04, 12pm-1pm</h1>'+
         '<h1 class="readerHeading">Reader: Norman Bates</h1>'+
@@ -13,40 +13,5 @@ var description7_4 =
       '</div>'+ //dialog_text_div
       '<div class="dialog_image_div">'+
         '<img src="img/and_then_there_were_none.jpg" style="" alt="book cover">' +
-      '</div>'+ //dialog_image_div
-    '</div>';
-
-var description7_5 =
-    '<div class="dialog">'+
-      '<div class="dialog_text_div">'+
-        '<h1 class="titleHeading">Charlotte\'s Web</h1>'+
-        '<h1 class="locationHeading">Location: 3100 Ellwood Ave</h1>'+
-        '<h1 class="dateHeading">Date: 07/05, 1pm-2pm</h1>'+
-        '<h1 class="readerHeading">Reader: Delta Macklin</h1>'+
-        '<div>'+
-          '<div style="text-align: justify;">' +
-            '<p style="padding-right: 10px;">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.</p>'+
-          '</div>'+
-        '</div>'+
-      '</div>'+ //dialog_text_div
-      '<div class="dialog_image_div">'+
-        '<img src="img/charlottes_web.jpg" style="" alt="book cover">' +
-      '</div>'+ //dialog_image_div
-    '</div>';
-var description7_6 =
-    '<div class="dialog">'+
-      '<div class="dialog_text_div">'+
-        '<h1 class="titleHeading">Charlotte\'s Web</h1>'+
-        '<h1 class="locationHeading">Location: 3100 Ellwood Ave</h1>'+
-        '<h1 class="dateHeading">Date: 07/05, 1pm-2pm</h1>'+
-        '<h1 class="readerHeading">Reader: Amy O\'Connor</h1>'+
-        '<div>'+
-          '<div style="text-align: justify;">' +
-            '<p style="padding-right: 10px;">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text.</p>'+
-          '</div>'+
-        '</div>'+
-      '</div>'+ //dialog_text_div
-      '<div class="dialog_image_div">'+
-        '<img src="img/charlottes_web.jpg" style="" alt="book cover">' +
       '</div>'+ //dialog_image_div
     '</div>';
