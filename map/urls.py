7e2934f19b7a41670ab6befeb3c780f9180e7398
Max Longton <elongton@vt.edu>
2018-07-01 13:18:17 -0400

    added the model and template view

--- map/urls.py
+++ map/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-
-from . import views
+from .views import MapView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', MapView.as_view(), name='home'),
 ]
--- map/views.py
+++ map/views.py
@@ -1,5 +1,21 @@
-from django.http import HttpResponse
+from django import forms
+from django.http import HttpResponseForbidden, HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic.edit import FormMixin
+from django.shortcuts import render
+from django.views.generic import (View, TemplateView,
+                                  CreateView, DetailView,
+                                  ListView)
+from .models import Reading
 
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the map index.")
+
+
+class MapView(TemplateView):
+
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['readings'] = Reading.objects.all()
+        return context
