7da3d368220135b3a5953d20f46ff0546ac8395a
Max Longton <elongton@vt.edu>
2018-07-03 17:30:42 -0400

    changed random number generator to affect long and lat and also decreased variation

--- map/templatetags/random_numbers.py
+++ map/templatetags/random_numbers.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 @register.filter(name="random_int")
 def random_int(value):
-    randadd = random.random()*1/1e2
+    randadd = random.random()*1/1e4
     value = value + randadd
     return value
